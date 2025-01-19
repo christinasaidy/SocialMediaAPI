935036d6179746f6b5129887c693500d063d4a90
Hani <hani.z.abdelghani@net.usek.edu.lb>
2025-01-19 20:12:20 +0200

    removed comment from user resource

--- SocialMedia.API/Resources/UserResources/UserResource.cs
+++ SocialMedia.API/Resources/UserResources/UserResource.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string UserName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string Role { get; set; } = string.Empty;  // Assuming Role is a property in Users
+        public string Role { get; set; } = string.Empty; 
     }
 }
--- SocialMediaAPI.infrastructure/obj/Debug/net8.0/SocialMediaAPI.infrastructure.AssemblyInfo.cs
+++ SocialMediaAPI.infrastructure/obj/Debug/net8.0/SocialMediaAPI.infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialMediaAPI.infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+584634d8973b354c3ef3e75ec95aac4e092b88b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3955665f15b3b95a93fa7acab1b42265073e001")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialMediaAPI.infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialMediaAPI.infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
